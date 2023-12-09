b1ec4eaa7a80317d3882bf86e2733528ad766099
Hanterok8 <sevcukandrij354@gmail.com>
2023-12-09 23:52:17 +0200

    First/Jump

--- Assets/Scripts/ManController.cs
+++ Assets/Scripts/ManController.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class ManController : MonoBehaviour
 {
+    [SerializeField] public GameObject _player;
     [SerializeField] public float _speed = 3f;
     [SerializeField] public float _maxSpeed = 4f;
     [SerializeField] public float _minSpeed = 2f;
     [SerializeField] private bool _ground;
-    [SerializeField] private float _jumpPower = 700f;
+    [SerializeField] private float _jumpPower = 4f;
     [SerializeField] public Animator _animator;
     public int _state;
 
@@ -19,6 +20,7 @@ public class ManController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
+            _player.GetComponent<CrouchControlller>().enabled = false;
             transform.Translate(Vector3.forward * _speed * Time.deltaTime);
             _state = 1;
             if (Input.GetKey(KeyCode.LeftShift))
@@ -29,26 +31,38 @@ public class ManController : MonoBehaviour
         }
         else
         {
+            _player.GetComponent<CrouchControlller>().enabled = true;
             _state = 0;
         }
         if (Input.GetKey(KeyCode.A))
         {
+            _player.GetComponent<CrouchControlller>().enabled = false;
             transform.Translate(Vector3.left * _minSpeed * Time.deltaTime);
             _state = 6;
         }
         if (Input.GetKey(KeyCode.D))
         {
+            _player.GetComponent<CrouchControlller>().enabled = false;
             transform.Translate(Vector3.right * _minSpeed * Time.deltaTime);
             _state = 5;
         }
         if (Input.GetKey(KeyCode.S))
         {
+            _player.GetComponent<CrouchControlller>().enabled = false;
             transform.Translate(Vector3.back * _minSpeed * Time.deltaTime);
             _state = 3;
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jumb();
+        }
         _animator.SetInteger("State", _state);
     }
-
+    void Jumb()
+    {
+        _animator.SetTrigger("Jump");
+        Rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Wall")
