a1d898f07d35a140f9d199fece08e315eb9c635e
Hanterok8 <140514675+Hanterok8@users.noreply.github.com>
2024-03-17 23:38:20 +0200

    ochi zacrivayotsa

--- Assets/ScrimerController2.cs
+++ Assets/ScrimerController2.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 
 public class ScrimerController2 : MonoBehaviour
 {
+    [SerializeField] private AudioSource _sourse;
     [SerializeField] private Animator _scrimerAnim;
+    [SerializeField] private Light _light;
     [SerializeField] private GameObject _gameObject;
+    [SerializeField] private GameObject _cube;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             StartCoroutine(ScrimerCD());
+            _light.enabled = false;
+            _sourse.Play();
         }
     }
     private IEnumerator ScrimerCD()
@@ -19,5 +24,8 @@ public class ScrimerController2 : MonoBehaviour
         yield return new WaitForSeconds(1);
         _scrimerAnim.SetBool("isTrrigerScrim", false);
         Destroy(_gameObject,2);
+        yield return new WaitForSeconds(3);
+        _light.enabled = true;
+        Destroy(_cube);
     }
 }
