bdcccfb549684526780715e45f2ceb919ad8b1d7
Hanterok8 <140514675+Hanterok8@users.noreply.github.com>
2024-03-18 18:39:43 +0200

    Labirint

--- Assets/ScrimerController2.cs
+++ Assets/ScrimerController2.cs
@@ -5,7 +5,7 @@ public class ScrimerController2 : MonoBehaviour
 {
     [SerializeField] private AudioSource _sourse;
     [SerializeField] private Animator _scrimerAnim;
-    [SerializeField] private Light _light;
+    [SerializeField] private Light[] _light;
     [SerializeField] private GameObject _gameObject;
     [SerializeField] private GameObject _cube;
 
@@ -14,7 +14,10 @@ public class ScrimerController2 : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             StartCoroutine(ScrimerCD());
-            _light.enabled = false;
+            foreach (Light light in _light)
+            {
+                light.enabled = false;
+            }
             _sourse.Play();
         }
     }
@@ -25,7 +28,10 @@ public class ScrimerController2 : MonoBehaviour
         _scrimerAnim.SetBool("isTrrigerScrim", false);
         Destroy(_gameObject,2);
         yield return new WaitForSeconds(3);
-        _light.enabled = true;
+        foreach (Light light in _light)
+        {
+            light.enabled = true;
+        }
         Destroy(_cube);
     }
 }
